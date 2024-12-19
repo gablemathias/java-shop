7079b1529de5ad641621f5eb24eb5950512c86dc
Gabriel Mathias <gablemathias@icloud.com>
2024-12-19 22:46:15 +0000

    feat(class): add ShoppingCart objects
    ShoppingCart is where you and and manage your items

---
+++ src/main/java/com/imalittletester/ShoppingCart.java
@@ -0,0 +1,56 @@
+package com.imalittletester;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+    private Map<String, Item> cart;
+
+    public ShoppingCart() {
+        this.cart = new HashMap<>();
+    }
+
+    public void add(String product, int price) {
+        if(price > 0) {
+            if(cart.get(product) == null) {
+                cart.put(product, new Item(product, 1, price));
+            } else {
+                cart.get(product).increaseQuantity();
+            }
+        }
+    }
+
+    public int price() {
+        int total = 0;
+        for(Item product : cart.values()) {
+            total += product.price();
+        };
+
+        return total;
+    }
+
+    public void print() {
+        for(Item product : cart.values()) {
+            System.out.println(product);
+        }
+    }
+
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        cart.add("milk", 3);
+        cart.print();
+        System.out.println("cart price: " + cart.price() + "\n");
+
+        cart.add("buttermilk", 2);
+        cart.print();
+        System.out.println("cart price: " + cart.price() + "\n");
+
+        cart.add("milk", 3);
+        cart.print();
+        System.out.println("cart price: " + cart.price() + "\n");
+
+        cart.add("milk", 3);
+        cart.print();
+        System.out.println("cart price: " + cart.price() + "\n");
+    }
+}
